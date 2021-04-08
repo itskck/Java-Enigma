6b89d1d89d8613ee541c6ad358de7cf21c65d3e8
Juliusz Piskor <juliuszpiskor@gmail.com>
2021-04-08 21:32:31 +0200

    Jpanel

--- src/com/company/Panel.java
+++ src/com/company/Panel.java
@@ -2,36 +2,61 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
-public class Panel {
+public class Panel extends JPanel {
     private JPanel panel=new JPanel();
     private JButton[] buttonArray = new JButton[26];
-
+    private ImageIcon icon = new ImageIcon("Bez nazwy.png");
     Panel(){
+        panel.setBackground(new Color(56,56,56));
+        panel.setLayout(null);
         panel.setPreferredSize(new Dimension(800,600));
         int wiersz=0;
         int kolumna=0;
+        int adj=0;
         for(int i=0; i<26;i++)
         {
-
-            if(i>9 && i<17) wiersz=30;
-            if(i>=17) wiersz= 60;
-            buttonArray[i] = new JButton(String.valueOf(i));
-            buttonArray[i].setBounds(kolumna,wiersz,200,20);
-          // buttonArray[i].setOpaque(false);
-          //  buttonArray[i].setContentAreaFilled(false);
-          //  buttonArray[i].setBorderPainted(false);
-           // buttonArray[i].setFocusPainted(false);
+            if(i>8 && i<=16) {
+                wiersz = 60;
+                adj=30;
+            }
+            else{
+                adj=0;
+            }
+            if(i>=17) wiersz= 120;
+            if(i==9 || i==17) kolumna=0;
+            ////////////////////////////////////////////////////////////////////////////////////////////
+            buttonArray[i] =
+            buttonArray[i] = new JButton(icon);
+            buttonArray[i].setText(String.valueOf(i));
+            buttonArray[i].setHorizontalTextPosition(JButton.CENTER);
+            buttonArray[i].setVerticalAlignment(JButton.CENTER);
+            buttonArray[i].setForeground(Color.WHITE);
+            buttonArray[i].setBounds(100+kolumna+adj,350+ wiersz,50,50);
+            buttonArray[i].setOpaque(false);
+            buttonArray[i].setContentAreaFilled(false);
+            buttonArray[i].setBorderPainted(false);
+            buttonArray[i].setFocusPainted(false);
             panel.add( buttonArray[i]);
-            kolumna+=30;
-            if(kolumna>30*9) kolumna=0;
+            int finalI = i;
+            buttonArray[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println(buttonArray[finalI].getText());
+                }
+            });
+            kolumna+=60;
             panel.repaint();
         }
 
     }
 
+
     public JPanel getPanel() {
         return panel;
     }
 }
+
